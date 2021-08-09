218250f1b7cbcc1b0017f477b82bc78683879f63
Ben Greene <Ben.Greene@digitalreasoning.com>
2021-08-09 02:40:03 +0100

    Cleanup

--- src/uservice.rs
+++ src/uservice.rs
@@ -22,24 +22,11 @@ pub fn start(config: &UServiceConfig) {
         .enable_all()
         .build()
         .unwrap();
-        // .expect("build runtime");
-
-    // rt.block_on(health_listen("health", 7979, &liveness));
-
-    // let local = tokio::task::LocalSet::new();
-    // local.block_on(&rt, health_listen("health", 7979, &liveness));
-
 
     // This creates the async functions from a non-awsync function
     rt.block_on(async {
-        println!("hello");
-
-        let local = tokio::task::LocalSet::new();
-        local.run_until(async move {
-            println!("GOT HRERE");
-            health_listen("health", 7979, &liveness).await;
-            println!("DONE");
-        }).await;
+        // ToDo: Look at this for clue on how to run on LocalSet : https://docs.rs/tokio/1.9.0/tokio/task/struct.LocalSet.html
+        health_listen("health", 7979, &liveness).await;
     });
 
 
